3d2e329193f1500087c2b647d33554cd6c0e6585
ajayp230199 <ajaypardhi.com@gmail.com>
2022-06-26 21:54:43 +0530

    lenth of text

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-//lert("Loding");
+//alert("Loding");
 function addNewWEField() {
-console.log("Adding new field");
+//console.log("adding new field");
+
+
+let newnode = document.createElement("textarea");
+newnode.classList.add("form-control");
+newnode.classList.add("weField");
+newnode.classList.add("mt-2")
+
+newnode.setAttribute("rows" , 2);
+newnode.setAttribute("placeholder" , "Enter here")
+
+
+let weOb = document.getElementById("we");
+let weAddButtonOb=document.getElementById("weAddButton");
+
+weOb.insertBefore(newnode,weAddButtonOb);
+}
+
+function addNewAQField()
+{
+    let newnode = document.createElement("textarea");
+    newnode.classList.add("form-control");
+    newnode.classList.add("eqField");
+    newnode.classList.add("mt-2")
+    
+    newnode.setAttribute("rows" , 2);
+    newnode.setAttribute("placeholder" , "Enter here")
+    
+    
+    let aqOb = document.getElementById("aq");
+    let aqAddButtonOb=document.getElementById("aqAddButton");
+    
+    aqOb.insertBefore(newnode,aqAddButtonOb);
 }
